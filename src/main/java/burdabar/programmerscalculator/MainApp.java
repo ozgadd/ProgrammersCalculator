1628d94e2802af5d6759b28c14f2e4d602f13aef
Bartosz Burda <bartoszburda93@gmail.com>
2016-12-15 23:49:28 +0100

    Add fxml gui and controller
    Add calculator gui and controller for fxml file - only field
    declaration, handling methods required

--- src/main/java/burdabar/programmerscalculator/MainApp.java
+++ src/main/java/burdabar/programmerscalculator/MainApp.java
@@ -12,13 +12,14 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Calculator.fxml"));
         
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
         
-        stage.setTitle("JavaFX and Maven");
+        stage.setTitle("Programmer Calculator");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
---
+++ src/main/java/burdabar/programmerscalculator/controller/CalculatorController.java
@@ -0,0 +1,102 @@
+package burdabar.programmerscalculator.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class CalculatorController implements Initializable {
+    
+    
+    @FXML
+    private Button shiftToRollButton;
+    @FXML
+    private Button moduloButton;
+    @FXML
+    private Button clearOperationButton;
+    @FXML
+    private Button delButton;
+    @FXML
+    private Button divideButton;
+    @FXML
+    private Button AButton;
+    @FXML
+    private Button BButton;
+    @FXML
+    private Button CButton;
+    @FXML
+    private Button DButton;
+    @FXML
+    private Button EButton;
+    @FXML
+    private Button FButton;
+    @FXML
+    private Button zeroButton;
+    @FXML
+    private Button oneButton;
+    @FXML
+    private Button twoButton;
+    @FXML
+    private Button threeButton;
+    @FXML
+    private Button fourButton;
+    @FXML
+    private Button fiveButton;
+    @FXML
+    private Button sixButton;
+    @FXML
+    private Button sevenButton;
+    @FXML
+    private Button eightButton;
+    @FXML
+    private Button nineButton;
+    @FXML
+    private Button multiplyButton;
+    @FXML
+    private Button minusButton;
+    @FXML
+    private Button equalButton;
+    @FXML
+    private Button addButton;
+    @FXML
+    private Button lengthButton;
+    @FXML
+    private Button leftShiftButton;
+    @FXML
+    private Button rightShiftButton;
+    @FXML
+    private Button orButton;
+    @FXML
+    private Button andButton;
+    @FXML
+    private Button xorButton;
+    @FXML
+    private Button notButton;
+    @FXML
+    private Button hexButton;
+    @FXML
+    private Button decButton;
+    @FXML
+    private Button octButton;
+    @FXML
+    private Button binButton;
+    @FXML
+    private Label hexField;
+    @FXML
+    private Label octField;
+    @FXML
+    private Label decField;
+    @FXML
+    private Label binField;
+    @FXML
+    private Label calcField;
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
